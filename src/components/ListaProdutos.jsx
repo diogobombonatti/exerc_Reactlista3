a21c12165d3b21d7cbff57654bcf5313cfeb0877
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-01 22:37:14 -0300

    Produto e lista produtos  jsx alterados

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,8 @@
+export const ListaProdutos = [
+
+{id:1,nome:"Pendrive",desc:"16GB USB 3.0",img:"https://picsum.photos/100/100",preco:80},
+{id:2,nome:"Teclado",desc:"16GB USB 3.0",img:"https://picsum.photos/100/100",preco:80},
+{id:3,nome:"Cadeira Gamer",desc:"16GB USB 3.0",img:"https://picsum.photos/100/100",preco:80}
+
+
+];
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,6 +1,43 @@
 export default function Produtos() {
-    return (
-      <div>Produtos</div>
-    )
-  }
-  
+    
+  React.useEffect(() => {
+    document.title = "Produtos";
+  }, []); // Defina o título da página no useEffect com um array de dependências vazio
+
+  return (
+    <>
+      <h1>Lista de Produtos</h1>
+      <div>
+        <table>
+          <thead> {/* Use <thead> para cabeçalhos da tabela */}
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>DESCRIÇÃO</th>
+              <th>IMAGEM</th>
+              <th>PREÇO</th>
+              <th>EDITAR / EXCLUIR</th> {/* Corrija a formatação do texto */}
+            </tr>
+          </thead>
+          <tbody> {/* Use <tbody> para o corpo da tabela */}
+            {produtos.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td>
+                  <img src={item.img} alt={item.desc} />
+                </td>
+                <td>{item.preco}</td>
+                <td>
+                  <button>Editar</button> {/* Adicione botões para editar/excluir */}
+                  <button>Excluir</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
