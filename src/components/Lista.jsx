e79db8c544e61b62a8e0ead51fc4db7f0088c5b5
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-01 21:09:15 -0300

    nomes dos produtos alterados para carros em lista

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -9,9 +9,9 @@ export default function Lista(){
             <Cabecalho>        
                     <li><Link to='/'>Home</Link> </li>
                     <li><Link to='/produtos'>Produtos</Link> </li>
-                    <li><Link to='/editar/produtos/1'>Produto - 1</Link> </li>
-                    <li><Link to='/editar/produtos/2'>Produto - 2</Link> </li>
-                    <li><Link to='/editar/produtos/3'>Produto - 3</Link> </li>
+                    <li><Link to='/editar/produtos/1'>Nissan GTR - 1</Link> </li>
+                    <li><Link to='/editar/produtos/2'>SUBARU WRX - 2</Link> </li>
+                    <li><Link to='/editar/produtos/3'>VOLKSWAGEN Passat - 3</Link> </li>
             </Cabecalho>
             
         </>
